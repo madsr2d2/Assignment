cc3c6b6fdf57c3d17fe87dd6d8df2476f7e7b047
madsr2d2 <mads.richardt@gmail.com>
2022-08-26 21:22:46 +0200

    mads

---
+++ Exercie_1a.c
@@ -0,0 +1,12 @@
+// Course: Hardware-oriented programming
+// Exercies: 1a
+// Student name: Mads Richardt
+// Date: 26.08.2022
+// Description: A program that prints the string "Programering er en kontaktsport."
+
+#include <stdio.h>
+
+int main(void) {
+    char s[] = "Programering er en kontaktsport."; // Initializing string
+    printf("%s\n",s); // Printing string to standard output
+}
