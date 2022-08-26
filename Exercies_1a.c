09a01f2cf02b0b885100b1dd8c5107437403c982
madsr2d2 <mads.richardt@gmail.com>
2022-08-26 17:02:05 +0200

    mads

---
+++ Exercies_1a.c
@@ -0,0 +1,9 @@
+//HNP_Exercise_1a
+// A program that prints the string "Programering er en kontaktsport".
+
+#include <stdio.h>
+
+int main(void) {
+    char s[] = "Programering er en kontaktsport."; // Initializing the string
+    printf("%s\n",s); // Printing string to standard output
+}
