436759e52c45611ba62611f0e39a5c40670630fb
madsr2d2 <mads.richardt@gmail.com>
2022-10-10 17:11:32 +0200

    mads

---
+++ A7/src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	char* article[] = {"den", "en", "nogle", "enhver, ”det", "flere"};
+
+	char* noun[] = {"personer", "studerende", "arbejdsløse", "by",  "og", "cykel", "s-tog"}; 
+
+	char* verb[]={ "kørte", "hoppede", "løb", "gik", "sad", "bevaegede"};
+
+	char* preposition[]={"til", "fra", "over", "under", "på", "ved siden af"};
+
+	printf("Hello World!\n");
+
+	return (0);
+}
