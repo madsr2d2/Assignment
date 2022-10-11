9ac6f6514c1995bfb3b8cd9de5f5e13c7aa6f5cd
madsr2d2 <mads.richardt@gmail.com>
2022-10-11 16:31:58 +0200

    mads

--- A7/src/main.c
+++ A7/src/main.c
@@ -1,16 +1,68 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define countOf(arr) (sizeof(arr)/sizeof(arr[0])) // Gets number of elements in array
+
+// Word arrays
+const char* article[] = {"den", "en", "nogle", "enhver", "det", "flere"};
+const char* noun[] = {"personer", "studerende", "arbejdsløse", "by", "og", "cykel", "s-tog"};
+const char* verb[] = {"korte", "hoppede", "lob", "gik", "sad", "bevægede"};
+const char* preposition[] = {"til", "fra", "over", "under", "på", "ved siden af"};
+
+const int maxWordLength = 11; // Length of longest word
+const int sentenceLength = 6; // words in sentence
+const int sentenceNumber = 50; // Number of sentences
+
+
+char* randomSentenceGenerator();
 
 int main()
 {
-	char* article[] = {"den", "en", "nogle", "enhver, ”det", "flere"};
+	srand(time(0));
 
-	char* noun[] = {"personer", "studerende", "arbejdsløse", "by",  "og", "cykel", "s-tog"}; 
+	for(int i = 0; i <= sentenceNumber; i++) {
+        char* sentence = randomSentenceGenerator();
+        printf("%s\n", sentence);
+        free(sentence);
+    }
 
-	char* verb[]={ "kørte", "hoppede", "løb", "gik", "sad", "bevaegede"};
-
-	char* preposition[]={"til", "fra", "over", "under", "på", "ved siden af"};
-
-	printf("Hello World!\n");
-
-	return (0);
+	return 0;
 }
+
+char* randomSentenceGenerator() {
+	char* sentence = calloc((sentenceLength*maxWordLength + sentenceLength), sizeof(char)); // Allocate memory
+
+  	// Construct sentence
+  	strcat(sentence, article[rand()%countOf(article)]); // Add article
+  	strcat(sentence, " ");
+
+  	strcat(sentence, noun[rand()%countOf(noun)]); // Add noun
+  	strcat(sentence, " ");
+
+  	strcat(sentence, verb[rand()%countOf(verb)]); // Add verb
+  	strcat(sentence, " ");
+
+  	strcat(sentence, preposition[rand()%countOf(preposition)]); // Add preposition
+  	strcat(sentence, " ");
+
+  	strcat(sentence, article[rand()%countOf(article)]); // Add article
+  	strcat(sentence, " ");
+
+  	strcat(sentence, noun[rand()%countOf(noun)]); // Add noun
+  	strcat(sentence, ".");
+
+  	sentence[0] = toupper(sentence[0]); // Capitalize first letter
+
+	return sentence;
+}
+
+
+
+
+
+
+
+
