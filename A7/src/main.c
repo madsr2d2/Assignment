00bfe9cde42bd41edb9f1d4c3bf10fdb385f60c1
madsr2d2 <mads.richardt@gmail.com>
2022-10-15 07:34:35 +0200

    mads

--- A7/src/main.c
+++ A7/src/main.c
@@ -1,7 +1,5 @@
 // code adapted from example found at https://codereview.stackexchange.com/questions/23845/random-sentences-generator
 
-
-
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
