67f2d38082412c88dea64ca9e9cecf5c2e52cc37
madsr2d2 <mads.richardt@gmail.com>
2022-10-24 14:23:29 +0200

    mads

---
+++ A8/src/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define name
+int main()
+{
+	printf("Hello World!\n");
+
+	typedef struct 
+	{
+		char Name[100];
+		char Surname[100];
+		int 
+	} Sunhedsprofil;
+
+
+	return (0);
+}
