cf995e2879c194f0d1e6f21becf7c5a3465276f8
madsr2d2 <mads.richardt@gmail.com>
2022-10-25 16:53:17 +0200

    mads

--- A8/src/main.c
+++ A8/src/main.c
@@ -1,17 +1,75 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define name
-int main()
+#define NAME_LENGTH 100
+
+typedef struct {
+	float BMI;
+	float MaxPulse;
+	float TargetPulseInterval[2];
+} Sunhedsdata;
+
+typedef struct
+{
+	char Name[NAME_LENGTH];
+	char Surname[NAME_LENGTH];
+	int BirthDate[3];
+	float Height;
+	float Weight;
+	int Age;
+	Sunhedsdata sd;
+} Sunhedsprofil;
+
+Sunhedsprofil getPersonalInfo(void);
+
+
+int main() 
 {
 	printf("Hello World!\n");
 
-	typedef struct 
-	{
-		char Name[100];
-		char Surname[100];
-		int 
-	} Sunhedsprofil;
+	
 
+	//strcpy((&sp)->Name, "Mads");
 
-	return (0);
+	Sunhedsprofil sp = getPersonalInfo();
+
+	printf("%d",sp.BirthDate[0]);
+	
+
+	//struct tm tm;
+	//time_t t = time(NULL);
+	//struct tm *tm = localtime(&t);
+	//printf("%d", tm->tm_mon);
+	//return (0);
+}
+
+Sunhedsprofil getPersonalInfo(void) {
+
+	Sunhedsprofil sp;
+	
+	// Get First Name.
+	printf("Enter first name: ");
+	scanf("%s",sp.Name);
+
+	// Get Surname.
+	printf("\nEnter surname: ");
+	scanf("%s", sp.Surname);
+
+	// Get birth date.
+	printf("\nEnter date of birth (format: day-month-year): ");
+	scanf("%2d%*c%2d%*c%4d", &sp.BirthDate[0], &sp.BirthDate[1], &sp.BirthDate[2]);
+
+	// Maybe do a check on the date format here... 
+
+	// Get height.
+	printf("\nEnter height in meters: ");
+	scanf("%f", &sp.Height);
+
+	// Get weight.
+	printf("\nEnter weight in kg: ");
+	scanf("%f", &sp.Weight);
+
+	return sp;
 }
