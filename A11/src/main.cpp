1aa5b5b03f98e00a4229f04e4eb0f8a56c1a0eb6
madsr2d2 <mads.richardt@gmail.com>
2022-11-15 08:08:20 +0100

    mads

---
+++ A11/src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+#include "database.h"
+
+int main(void)
+{
+	//Person person("mads","richardt",41,"address 1",111);
+
+	Person person;
+	person.setFirstName("mads");
+	person.setLastName("Ric");
+	person.ge
+	person.view();
+	cout << "Hello world!" << endl;
+}
