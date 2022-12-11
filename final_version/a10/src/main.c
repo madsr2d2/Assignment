2c0675be7f1994cf6ca6967c2db2a81c6ff320cf
madsr2d2 <mads.richardt@gmail.com>
2022-12-11 11:49:20 +0100

    mads

--- final_version/a10/src/main.c
+++ final_version/a10/src/main.c
@@ -6,6 +6,7 @@
 #define MAX_DATA_BASE_LENGTH 100
 #define MAX_ADDRESS_LENGTH 100
 size_t dataBaseSize;
+bool sorted = false;
 
 // Struct to hold person data
 typedef struct Person {
@@ -19,61 +20,67 @@ typedef struct Person {
 // Function to load scv file into array of structs
 size_t loadScvFile(Person **personPtr, char *fileName, size_t maxSize);
 // Function to add person to array of structs
-void addPersonToCsvFile(char *filename, Person *dataBase[]);
+void addPersonToDataBase(char *filename, Person *dataBase[]);
 // Function to perform binary search on phone  number
-long binarySearch(Person **dataBase, size_t dataBaseSize);
+long binarySearch(Person **dataBase, size_t dataBaseSize, unsigned int phoneNumber);
 // Function to print Person
 void printPerson(Person *personPtr);
 // Function to print database
 void printDataBase(Person *dataBase[], size_t dataBaseSize);
 // Function to sort database using the bubble sort algorithm
 void bubbleSort(Person *dataBase[], size_t dataBaseSize);
-
-// Function to do binary search on database
   
 int main() {
 	char fileName[MAX_NAME_LENGTH];
 	Person *dataBase[MAX_DATA_BASE_LENGTH];
 	int controlVar = 0;
 	long index;
+	unsigned int phoneNumber;
 
-	puts("***********************");
-	puts("Welcome to Assignment 9");
-	puts("***********************");
+	puts("************************");
+	puts("Welcome to Assignment 10");
+	puts("************************");
 
 	printf("\nEnter data base name: ");
-	scanf("%s",fileName);							// Get scv file name from user
-	while(getchar() != '\n'){}; 									// Flush stdin buffer
+	scanf("%s",fileName);								// Get scv file name from user
+	while(getchar() != '\n'){};							// Flush stdin buffer
 	dataBaseSize = loadScvFile(dataBase, 
-		fileName, MAX_DATA_BASE_LENGTH);			// Load scv file into database and initialize dataBaseSize
+		fileName, MAX_DATA_BASE_LENGTH);				// Load scv file into database and initialize dataBaseSize
 
 	do {
-		printf("\n1:Print database\n2:Add person to database\n3:Search database on Phone Number.\n4: Close Program.\nPlease choose option: ");
-		scanf("%1d", &controlVar);					// Get controlVar value from user
+		printf("\n1:Print database\n2:Add person to database\n3:Search database on Phone Number.\n4:Close Program.\nPlease choose option: ");
+		scanf("%1d", &controlVar);						// Get controlVar value from user
 		while(getchar() != '\n'){};
 		puts("");
 		switch (controlVar) {
 			case 1: 
-				printDataBase(dataBase, dataBaseSize);								// Add person to csv file if controlVar = 1
+				printDataBase(dataBase, dataBaseSize);	// Add person to csv file if controlVar = 1
 				break;	
-			case 2:
-				addPersonToCsvFile(fileName,dataBase);
+			case 2:										// Add person to data base if controlVar = 2
+				addPersonToDataBase(fileName,dataBase);
 				break;
-			case 3: 								// Find person in database on phone number if controlVar = 2
-				//if ((index = findPersonFromPhoneNumber(dataBase, dataBaseSize)) < 0) {
-				//	puts("\nNo match found!");		
-				//	break;
-				//}
-				//else {
-				//	puts("\nMatch found!");
-				//	printPerson(dataBase[index]);	// Print person if match found
-				//}
-				bubbleSort(dataBase,dataBaseSize); 
-				break;
-			default:
+			case 3:										// Find person in database on phone number if controlVar = 3
+				if (sorted == false) {
+					bubbleSort(dataBase,dataBaseSize);	// Sort database if not sorted
+					sorted = true; 						// Set sorted = true
+				}
+				
+				printf("Enter Phone Number To Search For: ");
+				scanf("%d", &phoneNumber);				// Get phone number from user
+				while (getchar() != '\n'){}				// Flush stdin
+				index = binarySearch(dataBase, 
+					dataBaseSize, phoneNumber);			// Do binary search for phone number
+				if (index == -1) {
+					puts("\nNo match found!");			// Print if no match found
+				}
+				else {
+					puts("\nMatch:");
+					puts("*******");
+					printPerson(dataBase[index]);		// Print match if match found
+					puts("\n*******\n");
+				}	
 				break;
 		}
-		
 	} while (controlVar != 4);
 
 	puts("Program closing...");
@@ -115,7 +122,7 @@ size_t loadScvFile (Person **dataBase, char *fileName, size_t maxSize) {
 	return counter; 										// Return number of lines scanned
 }
 
-void addPersonToCsvFile (char *filename, Person *dataBase[]) {
+void addPersonToDataBase (char *filename, Person *dataBase[]) {
 	FILE *fPtr = fopen(filename, "a");							// Open file in append mode
 	Person *person = (Person *)malloc(sizeof(Person));			// Creat person in heap
 
@@ -144,36 +151,33 @@ void addPersonToCsvFile (char *filename, Person *dataBase[]) {
 		person->address, person->phoneNumber); 					// Append person to SCV file.
 	fclose(fPtr);												// Close file
 	dataBaseSize++;												// Update database size
+	sorted = false;												// Set sorted = false
 	dataBase[dataBaseSize-1] = person;							// Add person pointer to database
 }	
 
-long binarySearch(Person **dataBase, size_t size) {
-	unsigned int phoneNumber;
-	unsigned int currentPhoneNumber;
+long binarySearch(Person **dataBase, size_t size, unsigned int phoneNumber) {
+	size_t l = 0;
+	size_t r = size - 1;
 
-	printf("Enter Phone Number To Search For: ");
-	scanf("%d", &phoneNumber); 									// Get phone number from user
-	while(getchar() != '\n'){};												// Flush stdin 
-
-	for (size_t i = 0; i < size; i++) {							// Loop through dataBase
-			currentPhoneNumber = dataBase[i]->phoneNumber;		// Get current phone number
-			printf("%d",currentPhoneNumber);
-			if (currentPhoneNumber == phoneNumber) {
-				return (long) i;								// Return person index if match found
-				printf("%d",currentPhoneNumber);
-			}
+	while (l <= r) {
+		size_t m = l + (r - l)/2;
+		if (dataBase[m]->phoneNumber == phoneNumber)
+			return m;								// Return middle index if phone number is in middle
+		if (dataBase[m]->phoneNumber < phoneNumber)
+			l = m + 1;								// Search right half if phon number is larger than middle
+		else
+			r = m - 1;								// Search left half if phone number is smaller tha middle
 	}
-	return -1;													// Return -1 if no match found
+	return -1;										// Return -1 if no match found
 }
 
-void printPerson(Person *person){
+void printPerson(Person *person) {
 	printf("First name: %s\nLast name: %s\nAge: %d\nAddress: %s\nPhone number: %u",
 	person->firstName,person->lastName,person->age,person->address,person->phoneNumber);  
 }
 
-void printDataBase(Person *dataBase[], size_t dataBaseSize){
-	for (size_t i = 0; i < dataBaseSize; i++)
-	{	
+void printDataBase(Person *dataBase[], size_t dataBaseSize) {
+	for (size_t i = 0; i < dataBaseSize; i++) {
 		printf("Entry %lu:\n", i+1);
 		printPerson(dataBase[i]);
 		puts("\n");
